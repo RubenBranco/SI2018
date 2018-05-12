0a0b34bf7ba26367d16dcb91b65b1497f2865b4d
Ruben Branco <ruben.branco@outlook.pt>
2018-05-12 23:07:52 +0100

    Graph to string function

--- input_output.py
+++ input_output.py
@@ -1,3 +1,5 @@
+from operator import itemgetter
+
 def graph_from_file(file):
     row = 0
     column = 0
@@ -12,3 +14,14 @@ def graph_from_file(file):
             row += 1
             column = 0
     return graph
+
+def graph_to_string(graph):
+    keys = sorted(graph.keys())
+    string = ''
+
+    for r in range(keys[-1][0] + 1):
+        for c in range(keys[-1][1] + 1):
+            string += graph[(c, r)]
+        string += '\n'
+
+    return string
